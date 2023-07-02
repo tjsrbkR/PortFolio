caedb499f2b2d95af70e5b8f1b322a66a85c29d8
tjsrbKR <kkil5205@gmail.com>
2023-07-02 21:30:34 +0900

    멤버쉽 완성,faq제작

--- 040_MobileWeb_Custom/js/member.js
+++ 040_MobileWeb_Custom/js/member.js
@@ -2,3 +2,27 @@ $(".tab ul li").on("click", function () {
     $(".tab ul li").removeClass("on");
     $(this).addClass("on");
 });
+
+let tabMenu = document.querySelectorAll(".tab ul li");
+let mileage = document.querySelector(".mileage");
+let service = document.querySelector(".service");
+let cupone = document.querySelector(".cupone");
+service.style.display = "none";
+cupone.style.display = "none";
+for (let i = 0; i < tabMenu.length; i++) {
+    tabMenu[i].addEventListener("click", function () {
+        mileage.style.display = "none";
+        service.style.display = "none";
+        cupone.style.display = "none";
+        if (i == 0) {
+            mileage.style.display = "block";
+        }
+        else if (i == 1) {
+            service.style.display = "block";
+        }
+        else {
+            cupone.style.display = "block";
+        }
+    })
+}
+
