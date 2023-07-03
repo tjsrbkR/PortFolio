7524331e18a2d1cc1d2e84df5ca2ae77f34b7592
tjsrbkR <kkil5205@gmail.com>
2023-07-03 16:34:29 +0900

    폴더명 변경

--- 060_Cartier/js/cartier.js
+++
@@ -1,9 +0,0 @@
-const video = $("video");
-$("article").on("mouseover", function () {
-    $(this).stop().animate({ width: "35%" }, 300);
-    $(this).find("video").get(0).play();
-});
-$("article").on("mouseleave", function () {
-    $(this).stop().animate({ width: "12%" }, 300);
-    $(this).find("video").get(0).pause();
-});
